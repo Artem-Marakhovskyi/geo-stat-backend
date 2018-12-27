44071ccac930cd8a651409b4e5fb65d4733c8809
SabinaPashayeva <36380366+SabinaPashayeva@users.noreply.github.com>
2018-12-27 23:09:22 +0200

    add swagger

--- GeoStat/GeoStat/Startup.cs
+++ GeoStat/GeoStat/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace GeoStat
 {
@@ -26,6 +27,17 @@ namespace GeoStat
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "GeoStat API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = "None",
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +51,14 @@ namespace GeoStat
             {
                 app.UseHsts();
             }
+            
+            app.UseSwagger();
+            
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();

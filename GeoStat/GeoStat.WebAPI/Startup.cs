49dcecff224adc4c5483b9670aa2caf13bd89296
Artem-Marakhovskyi <Artem.marakhovskyi@gmail.com>
2019-01-12 12:34:08 +0000

    initial commit in webapi

---
+++ GeoStat/GeoStat.WebAPI/Startup.cs
@@ -0,0 +1,70 @@
+﻿using GeoStat.CrossCutting.Logger;
+using GeoStat.DataAccess;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeoStat.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<GeoStatContext>
+                    (opt => opt.UseSqlServer(connectionString));
+
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "GeoStat API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = "None",
+                });
+            });
+
+            var logger = GeoStatLogger.Factory.CreateLogger("GeoStat");
+            services.AddScoped<IGeoStatLogger>(serviceProvider => new GeoStatLogger(logger));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = "swagger";
+            });
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}

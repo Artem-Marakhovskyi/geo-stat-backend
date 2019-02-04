7e7364bee6dc95d85e542097fa5a061d4419b848
SabinaPashayeva <36380366+SabinaPashayeva@users.noreply.github.com>
2019-02-04 20:28:12 +0200

    update LocationDomainManager

--- GeoStat/GeoStat.BussinessLogic/LocationDomainManager.cs
+++ GeoStat/GeoStat.BussinessLogic/LocationDomainManager.cs
@@ -1,17 +1,78 @@
 ﻿using System.Net.Http;
+using System.Threading.Tasks;
 using GeoStat.DataAccess;
 using GeoStat.DTO;
 using GeoStat.Entities;
+using System.Linq;
 
 namespace GeoStat.BussinessLogic
 {
     public class LocationDomainManager : BaseDomainManager<LocationDto, Location>
     {
+        private GeoStatContext _geoStatContext;
+
         public LocationDomainManager(
             GeoStatContext geoStatContext,
             HttpRequestMessage requestMessage) 
             : base(geoStatContext, requestMessage)
         {
+            _geoStatContext = geoStatContext;
+        }
+
+        public IQueryable<Location> GetLocationsByUserId(string userId, string id)
+        {
+            if (userId == id || CheckPermissionsForGroupMember(userId, id))
+            {
+                var locationsOfUser = _geoStatContext.Locations.Where(l => l.UserId == id);
+                return locationsOfUser;
+            }
+
+            return null;
+        }
+
+        public IQueryable<Location> GetLocationsByGroupId(string userId, string groupId)
+        {
+            if (CheckPermissionsForGroup(userId, groupId))
+            {
+                var groupMembersId = _geoStatContext.GroupUsers
+                    .Where(u => u.GroupId == groupId)
+                    .Select(u => u.UserId);
+
+                var locationsOfGroupMembers = _geoStatContext.Locations
+                    .Where(l => groupMembersId.Contains(l.UserId));
+
+                return locationsOfGroupMembers;
+            }
+
+            return null;
+        }
+
+        private bool CheckPermissionsForGroup(string userId, string groupId)
+        {
+            var groupUsersId = _geoStatContext.GroupUsers
+                .Where(u => u.GroupId == groupId)
+                .Select(u => u.UserId);
+
+            if (groupUsersId.Contains(userId)) return true;
+
+            return false;
+        }
+
+        private bool CheckPermissionsForGroupMember(string userId, string groupMemberId)
+        {
+            var groupsOfMember = _geoStatContext.GroupUsers
+                .Where(u => u.UserId == groupMemberId)
+                .Select(u => u.GroupId);
+
+            foreach (var groupId in groupsOfMember)
+            {
+                if (CheckPermissionsForGroup(userId, groupId))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     } 
 }
